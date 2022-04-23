3f7fb66232586ec02e7df99637b03ff5bcea7815
batuhanaydogdu <batuhan.aydogdu0002@gmail.com>
2022-04-23 11:31:49 +0300

    project initialization

---
+++ story-reading-tracker-ws/src/main/java/com/impostors/app/ws/storyreadingtrackerws/StoryReadingTrackerWsApplication.java
@@ -0,0 +1,13 @@
+package com.impostors.app.ws.storyreadingtrackerws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryReadingTrackerWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryReadingTrackerWsApplication.class, args);
+	}
+
+}
---
+++ story-reading-tracker-ws/src/test/java/com/impostors/app/ws/storyreadingtrackerws/StoryReadingTrackerWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impostors.app.ws.storyreadingtrackerws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryReadingTrackerWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
