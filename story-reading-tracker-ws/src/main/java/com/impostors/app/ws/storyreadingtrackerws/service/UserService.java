13f40d618baf9d381f1f66aedf408ddcf6e0fb3c
EmreAkyildiz <73307332+EmreAkyildiz@users.noreply.github.com>
2022-05-28 23:59:19 +0300

    Update User

--- story-reading-tracker-ws/src/main/java/com/impostors/app/ws/storyreadingtrackerws/service/UserService.java
+++ story-reading-tracker-ws/src/main/java/com/impostors/app/ws/storyreadingtrackerws/service/UserService.java
@@ -7,5 +7,6 @@ public interface UserService extends UserDetailsService{
     UserDto createUser(UserDto user);
     UserDto getUserByEmail(String email);
     UserDto getCurrentUser();
+    UserDto updateUser(UserDto simpleUserDto);
 
 }
---
+++ story-reading-tracker-ws/src/main/java/com/impostors/app/ws/storyreadingtrackerws/ui/model/request/UserUpdateRequestModel.java
@@ -0,0 +1,41 @@
+package com.impostors.app.ws.storyreadingtrackerws.ui.model.request;
+
+public class UserUpdateRequestModel {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
